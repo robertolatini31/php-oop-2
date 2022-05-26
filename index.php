c9075c3f24ed43a67b20aa6115f5772b2a745869
robertolatini31 <robertolatini31@gmail.com>
2022-05-26 15:33:22 +0200

    add AnimalsGame class

--- index.php
+++ index.php
@@ -3,24 +3,24 @@ class AnimalsProduct {
     public $name;
     public $price;
     public $img;
-
-    function __construct(String $name, String $price, String $img){
+    public $species;
+    function __construct(String $name, String $price, String $img, String $species){
         $this->name = $name;
         $this->price = $price;
         $this->img = $img;
+        $this->species = $species;
     }
 }
-$newAnimalsProduct = new AnimalsProduct('cuccia', '19$', 'https://picsum.photos/200/300');
+$newAnimalsProduct = new AnimalsProduct('cuccia', '19$', 'https://picsum.photos/200/300', 'cane');
 var_dump($newAnimalsProduct);
 
  class AnimalsFood extends AnimalsProduct {
     public $type = 'food';
     public $wetFood;
     public $kibbleFood;
-    public $species;
+    
     function __construct(String $name, String $price, String $img , String $species, Bool $wetFood, Bool $kibbleFood){
-        parent::__construct($name, $price, $img);
-        $this->species = $species;
+        parent::__construct($name, $price, $img, $species);
         $this->wetFood = $wetFood;
         $this->kibbleFood = $kibbleFood;
     }
@@ -28,3 +28,19 @@ var_dump($newAnimalsProduct);
 
 $newAnimalsFood = new AnimalsFood('cuccia', '19$', 'https://picsum.photos/200/300', 'cane', true, false);
 var_dump($newAnimalsFood);
+
+
+class AnimalsGame extends Animalsproduct {
+    public $type = 'game';
+    public $rubberGame;
+    public $clothGame;
+    public $species;
+    function __construct(String $name, String $price, String $img , String $species, Bool $rubberGame, Bool $clothGame){
+        parent::__construct($name, $price, $img, $species);
+        $this->rubberGame = $rubberGame;
+        $this->clothGame = $clothGame;
+    }
+}
+
+$newAnimalsGame = new AnimalsGame('cuccia', '19$', 'https://picsum.photos/200/300', 'cane', true, false);
+var_dump($newAnimalsGame);
