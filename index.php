d8e1e742daacca246e06415a6259739318685c72
robertolatini31 <robertolatini31@gmail.com>
2022-05-26 15:24:53 +0200

    adjust AnimalsFood class

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ class AnimalsProduct {
     public $price;
     public $img;
 
-    function __construct($name, $price, $img){
+    function __construct(String $name, String $price, String $img){
         $this->name = $name;
         $this->price = $price;
         $this->img = $img;
@@ -15,12 +15,16 @@ var_dump($newAnimalsProduct);
 
  class AnimalsFood extends AnimalsProduct {
     public $type = 'food';
+    public $wetFood;
+    public $kibbleFood;
     public $species;
-    function __construct($name, $price, $img ,$species){
+    function __construct(String $name, String $price, String $img , String $species, Bool $wetFood, Bool $kibbleFood){
         parent::__construct($name, $price, $img);
         $this->species = $species;
+        $this->wetFood = $wetFood;
+        $this->kibbleFood = $kibbleFood;
     }
  }
 
-$newAnimalsFood = new AnimalsFood('cuccia', '19$', 'https://picsum.photos/200/300', 'cane');
+$newAnimalsFood = new AnimalsFood('cuccia', '19$', 'https://picsum.photos/200/300', 'cane', true, false);
 var_dump($newAnimalsFood);
